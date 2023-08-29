7df9ae5f5c45879ab27273eb30eef42b1b77fef1
matlencar <rm94642@fiap.com.br>
2023-08-29 14:51:28 -0300

    first commit for study tailwind and next

---
+++ project/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='p-8 bg-slate-900 text text-slate-100 h-screen'>
+    <h1 className='font-bold text-5xl text-violet-500-slate'>Hello World</h1>
+    <h2>Hello World</h2>
+    <p>Hello World</p>
+    </div>
+  )
+}
