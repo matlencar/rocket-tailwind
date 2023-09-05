746e60b0d1acc15fd00be79462a077d90c761857
matlencar <rm94642@fiap.com.br>
2023-09-05 15:17:59 -0300

    feat: init work tailwind

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -1,11 +1,19 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className='p-8 bg-slate-900 text text-slate-100 h-screen'>
-    <h1 className='font-bold text-5xl text-violet-500-slate'>Hello World</h1>
-    <h2>Hello World</h2>
-    <p>Hello World</p>
+    <div className="p-8 bg-slate-900 text text-slate-100 h-screen">
+      <h1 className="font-bold text-5xl flex items-center gap-3 before:w-0.5 before:h-8 before:sky-500 before:flex">
+        Hello World
+      </h1>
+      <h2>Hello World</h2>
+      <p>Hello World</p>
+      <button
+        disabled
+        className="bg-sky-500 px-2 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed"
+      >
+        Sign in
+      </button>
     </div>
-  )
+  );
 }
