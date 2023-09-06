1432a9d3aa926fd49638e052f2098a99276af991
matlencar <rm94642@fiap.com.br>
2023-09-06 15:22:42 -0300

    commit dark and light mode

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -2,18 +2,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="p-8 bg-slate-900 text text-slate-100 h-screen">
-      <h1 className="font-bold text-5xl flex items-center gap-3 before:w-0.5 before:h-8 before:sky-500 before:flex">
-        Hello World
-      </h1>
-      <h2>Hello World</h2>
-      <p>Hello World</p>
-      <button
-        disabled
-        className="bg-sky-500 px-2 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed"
-      >
-        Sign in
-      </button>
+    <div className="p-8 bg-slate-50 text text-slate-900 h-screen flex felx-col items-center text-center">
+      <div className="max-w-2xl">
+        <h1 className="font-bold text-3xl sm:text-5xl lg:text-6xl flex items-center gap-3 before:w-0.5 before:h-8 before:bg-sky-500 before:flex">
+          Hello Tailwind
+        </h1>
+        <h2>Hello World</h2>
+        <p>Hello World</p>
+        <button
+          disabled
+          className="bg-sky-500 px-2 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed"
+        >
+          Sign in
+        </button>
+      </div>
     </div>
   );
 }
